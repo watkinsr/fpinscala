dac411db7613026333c828b32d0bc4f7514629fd
Ryan Watkins <ryanwatkins54@gmail.com>
2018-09-22 17:32:02 +0100

    add: HOF implementations

---
+++ src/main/scala/example/HOF.scala
@@ -0,0 +1,32 @@
+class HigherOrderFunction {
+  def findFirst[A](as: Array[A], p: A => Boolean) : Int = {
+    @annotation.tailrec
+    def loop(n: Int): Int =
+      if (n >= as.length) -1
+      else if (p(as(n))) n
+      else loop(n + 1)
+
+    loop(0)
+  }
+
+  // Exercise 2.2
+  def isSorted[A](as: Array[A], ordered: (A,A) => Boolean) : Boolean = {
+    def loop(n: Int) : Boolean =
+      if (n >= as.length)
+        true
+      else if (n < as.length && ordered(as(n), as(n - 1)))
+        loop(n + 1)
+      else
+        false
+
+    loop(1)
+  }
+
+  def partial1[A,B,C](a: A, f: (A,B) => C): B => C =
+    (b: B) => f(a,b)
+
+  // Exercise 2.3
+  def curry[A,B,C](f: (A, B) => C) : A => (B => C) = {
+    (a: A) => partial1(a, f)
+  }
+}
