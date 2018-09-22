ec684195da2116e0398500065f51821baf013b8e
Ryan Watkins <ryanwatkins54@gmail.com>
2018-09-22 17:55:43 +0100

    add: rest of ch2 exercises

--- src/main/scala/example/HOF.scala
+++ src/main/scala/example/HOF.scala
@@ -23,10 +23,20 @@ class HigherOrderFunction {
   }
 
   def partial1[A,B,C](a: A, f: (A,B) => C): B => C =
-    (b: B) => f(a,b)
+    (b: B) => f(a, b)
 
   // Exercise 2.3
   def curry[A,B,C](f: (A, B) => C) : A => (B => C) = {
-    (a: A) => partial1(a, f)
+    a => b => f(a, b)
+  }
+
+  // Exercise 2.4
+  def uncurry[A,B,C](f: A => B => C) : (A, B) => C = {
+    (a, b) => f(a)(b)
+  }
+
+  // Exercise 2.5
+  def compose[A,B,C](f: B => C, g: A => B): A => C = {
+    a => f(g(a))
   }
 }
